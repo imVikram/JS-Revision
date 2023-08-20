4cca5880b5072ef3979e806065b78112ce2aa48f
Vikram Singh <vikramsingh03sep@gmail.com>
2023-08-20 17:46:34 +0000

    dtatype summary

---
+++ 1_Basics/5_DatatypesSummary.js
@@ -0,0 +1,32 @@
+//**Objects & Event listeners are most importatnt  */
+
+//***********Primitive**** */
+
+//7n types:(call by value)//
+//string,Number,boolean,null,undefined,Symbol,BigInt
+
+// const score =100.3
+// console.log(score);
+
+// const id =Symbol('123')
+// const Anotherid =Symbol('123')
+
+// console.table([id,Anotherid]);
+// console.log(id===Anotherid);//false
+
+//***Reference type(Non Primitive) ****/
+//Array,Objects,Functions
+// const heros=["shaktiman","naagraj","doga"]
+// console.log(heros.length);
+
+// let myObj = {
+//     name:"vikram",
+//     age :"20"
+// }
+// console.log(myObj);
+
+const myFunc = function(){
+console.log("Hey boy");
+}
+
+console.log(typeof(myFunc));
