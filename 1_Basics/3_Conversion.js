18fccdb7f312185e12a8d4838b86f403492420f0
Vikram Singh <vikramsingh03sep@gmail.com>
2023-08-20 16:36:44 +0000

    conversion of datatypes

---
+++ 1_Basics/3_Conversion.js
@@ -0,0 +1,30 @@
+// let score = 51
+
+// console.log(score);
+// console.log(typeof(score)); //number
+
+
+///////////////////////////////////////////////////////
+// let score = "51"
+// console.log(score);
+// console.log(typeof(score)); //string
+
+// let valueScore = Number(score)
+// console.log(typeof(valueScore)); //number
+
+///////////////////////////////////////////////////////
+
+// let score = "51ABC"
+// console.log(score);
+// console.log(typeof(score)); //string
+
+// let valueScore = Number(score)
+// console.log(typeof(valueScore)); //number
+
+////////////////////////////////////////////////
+
+let isLoggedIn = ""
+//when 1=>true, when 0 =>false, when "" => false, when "abc" => false
+let booeanLoggedIn = Boolean(isLoggedIn)
+console.log(booeanLoggedIn);
+
