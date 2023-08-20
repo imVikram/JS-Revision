5d8745a18d441f293b4879554dd920845a705ca5
Vikram Singh <vikramsingh03sep@gmail.com>
2023-08-20 16:25:21 +0000

    datatypes

---
+++ 1_Basics/Datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all js code as newer version of JS
+
+//alert(0+2)//We are using node.js not browser thats i did not work
+console.log(0+2);
+
+//data types (PREMETIVE)
+
+//number => 2 to the power 53
+//bigint
+//string => "vikram" //recommended 
+//boolean => true/false
+//null => standalone value (one of its kind)
+//undefined => not defined
+//symbol => to define uniqueness
+
+//data  types (Objects)
+//console.log(typeof(7)); //number
+
+//console.log(typeof(null)); //object
+console.log(typeof(undefined)); //undefined
