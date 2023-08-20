240930397eb70272e9c9155c4ce4b4249752b96c
Vikram Singh <vikramsingh03sep@gmail.com>
2023-08-20 17:18:03 +0000

    operations

--- 1_Basics/3_Conversion.js
+++ 1_Basics/3_Conversion.js
@@ -23,8 +23,49 @@
 
 ////////////////////////////////////////////////
 
-let isLoggedIn = ""
-//when 1=>true, when 0 =>false, when "" => false, when "abc" => false
-let booeanLoggedIn = Boolean(isLoggedIn)
-console.log(booeanLoggedIn);
+// let isLoggedIn = ""
+// //when 1=>true, when 0 =>false, when "" => false, when "abc" => false
+// let booeanLoggedIn = Boolean(isLoggedIn)
+// console.log(booeanLoggedIn);
+
+//**************Operations*************/
+// let value = 3
+// let negValue = -value
+// //console.log(negValue);  //-3
+
+// console.log(2+value); //5
+// console.log(3-value); //0
+// console.log(2*value); //6
+// console.log(2/value); //0.6666666666
+// console.log(3/value); //1
+
+//************************************* */
+// let str1 = "1"
+// let str2 = 2
+
+// let str3 = str1 + str2 
+// console.log(str3);//12
+
+// let str3 = str1 + str2 + 2
+// console.log(str3);//122
+
+// let num1, num2, num3
+// num1 = num2=num3 = 2*2
+// console.log(num2);  //4
+
+
+//*************PREFIX********* */
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter); //101
+
+//***************POSTFIX***********
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);//101
+
+
+
+
+
 
