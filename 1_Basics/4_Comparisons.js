544308bc5f95d04325d8462f997c59510c6c006c
Vikram Singh <vikramsingh03sep@gmail.com>
2023-08-20 17:29:27 +0000

    comparisons

---
+++ 1_Basics/4_Comparisons.js
@@ -0,0 +1,10 @@
+// console.log(3>4);//false
+// console.log(3<4);//true
+// console.log(3<"4");//true but not recommanded 
+
+// console.log(null>0) //false
+// console.log(null==0) //false
+// console.log(null>=0) //true
+
+console.log("2"===2) //false(strict checking(datatype))
+
