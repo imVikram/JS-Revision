b3cd14e708fcdb7a190e239ab6a835a7f208f3da
Vikram Singh <vikramsingh03sep@gmail.com>
2023-08-21 18:25:27 +0000

    date and time

---
+++ 1_Basics/8_Date.js
@@ -0,0 +1,23 @@
+//Dates
+
+let myDate = new Date()
+// console.log(myDate);//2023-08-21T18:11:54.008Z
+// console.log(myDate.toString());//Mon Aug 21 2023 18:13:24 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toJSON());//2023-08-21T18:13:24.773Z
+// console.log(myDate.toLocaleString());//8/21/2023, 6:13:24 PM
+// console.log(myDate.toISOString());//2023-08-21T18:13:24.773Z
+
+// let myCreatedDate = new Date(2023, 0,23)//Months starts from 0 index
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamps = Date.now()
+// console.log(myTimeStamps);
+// console.log(myTimeStamps);
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+// console.log(newDate.getMonth() +1);
+// console.log(newDate.getDay());
+
