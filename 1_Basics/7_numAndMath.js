61f57c62cac63ec9db64869a630aa95531d9020e
Vikram Singh <vikramsingh03sep@gmail.com>
2023-08-21 18:07:14 +0000

    maths

---
+++ 1_Basics/7_numAndMath.js
@@ -0,0 +1,35 @@
+// const score =309
+// console.log(score);
+
+// const balance = new Number(1000000)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(balance.toLocaleString())//1,000,000
+
+// const otherNum = 123.45679988
+// console.log(otherNum.toPrecision(5));
+
+// const hundreds = 100000000000
+// console.log(hundreds.toLocaleString('en-in')); //indian format
+// console.log(hundreds.toLocaleString());//default US
+
+
+
+
+//++++++++++++++++++++++++ Maths +++++++++++++++++++
+
+const hundreds = 100000
+// console.log(Math);
+// console.log(Math.round(4.499)); //4
+// console.log(Math.ceil(4.499)); //5
+// console.log(Math.floor(4.499)); //4
+
+//console.log(Math.min(2,4, 7,9,99));
+//console.log(Math.max(2,4, 7,9,99));
+
+//console.log((Math.random()*10) +1);
+const min=11
+const max =99
+console.log(Math.random() * ((max-min) + min));
+
