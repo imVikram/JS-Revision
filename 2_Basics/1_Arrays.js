60844ad93800052b67140ee1ff8a471bbb8f7793
Vikram Singh <vikramsingh03sep@gmail.com>
2023-08-22 23:36:02 +0000

    Arrays 1

---
+++ 2_Basics/1_Arrays.js
@@ -0,0 +1,46 @@
+//Array
+
+//const myArray = [0,1,2,3,4,5,6, true, "Vikram"]
+// console.log(myArray[8]);
+// console.log(myArray.length);
+
+// let newArray = ["Shaktimaan", "Vikramji"]
+// myArray = newArray;
+// console.log(newArray);
+
+//++++++++++++++++++++++Array Methods+++++++++++
+
+const myArray2 = new Array(1,3,5,7)
+//console.log(myArray2[4]);//undefined
+// console.log(myArray2[3])
+// myArray2.push(9)
+// console.log(myArray2)
+// myArray2.push(11)
+// console.log(myArray2)
+// myArray2.pop(11)
+// console.log(myArray2)
+// myArray2.unshift(100)
+// myArray2.shift()
+// console.log(myArray2)//[ 100, 1, 3, 5, 7 ]
+
+// console.log(myArray2.includes(7));
+// console.log(myArray2) //true
+
+// console.log(myArray2.indexOf(7));
+// console.log(myArray2)
+
+
+// const newArr = myArray2.join()
+// console.log(myArray2);
+// console.log(typeof(newArr));
+
+//Slice, Splice
+// console.log("A ",myArray2);
+// const myn1 = myArray2.slice(1,3)
+
+// console.log(myn1);
+// console.log("B ", myArray2);
+
+// const myn2 = myArray2.splice(1,3)
+// console.log("C ", myArray2);
+// console.log(myn2);
