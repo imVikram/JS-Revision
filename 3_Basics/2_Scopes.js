c0c39bae7d7ac8682362b1ba6c29bbc7b4e61524
Vikram Singh <vikramsingh03sep@gmail.com>
2023-08-25 04:43:37 +0000

    scopes & hoisting

--- 3_Basics/2_Scopes.js
+++ 3_Basics/2_Scopes.js
@@ -8,9 +8,59 @@ if (true) {
     let a=10
 const b=20
  //c=30
- console.log("inner: ", a);
+ //console.log("inner: ", a);
 }
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+//=========Nested Functions========
+
+function one() {
+    const username = "vikram"
+
+    function two() {
+        const website = "Youtube"
+        console.log(username);
+    }
+    //console.log(website);
+
+    //two()
+}
+
+//one()
+
+//===========if else function=======
+
+// if (true) {
+//     const username = "admin"
+//     if (username==="admin") {
+//         const website = " youtube"
+//         console.log(username + website);
+        
+//     }
+//     //console.log(website);
+// }
+// //console.log(username);
+
+//+++++++Interesting++++++++++++
+
+console.log(addOne(5));
+function addOne(value) {
+    return value + 1;
+
+}
+
+//===========Hoisting============
+
+onsole.log(addTwo(5));//error because of its declaration type
+
+//sometimes we can use it as
+const addTwo =  function (num) {
+    return num + 2;
+    
+}
+c
+
+
