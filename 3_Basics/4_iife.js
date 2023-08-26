9f2ad73ff54072e3da7d37b225505f32b0ac88fe
Vikram Singh <vikramsingh03sep@gmail.com>
2023-08-26 17:20:11 +0000

    IIFE fn

---
+++ 3_Basics/4_iife.js
@@ -0,0 +1,24 @@
+//Immediately invoked function expression IIFE
+
+// function one() {
+//     console.log(`DB Connected`);
+// }
+
+// one()
+
+//++++++IIFE+++++++
+
+// (function one() {
+    //named IIFE
+//     console.log(`DB Connected`);
+// })();//make sure to semicoln to end its context
+//in case we want to write 2 IIFE on single file 
+//and we forget to add semicolumn at the end of the 1st IIFE, it wont execute
+
+//Arrow finction way to write IIFE
+( (name) => {
+    //un-named IIFE
+    console.log(`DB Connected ${name}`);
+})('by Vikram')
+
+
