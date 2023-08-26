d986b81d6c462c365a15e0d17f6a5b09f2a104ed
Vikram Singh <vikramsingh03sep@gmail.com>
2023-08-26 17:09:27 +0000

    Arrow functions detailed

--- 3_Basics/3_Arrow.js
+++ 3_Basics/3_Arrow.js
@@ -3,7 +3,7 @@ const user ={
     price:999,
 
     welcomeMessage : function () {
-        console.log(`Welcome ${this.userName}`);
+        console.log(`Welcome ${this.userName}, you have paid ${user.price}, thank you!`);
         //console.log(this);
     }
 
@@ -12,4 +12,40 @@ const user ={
 // user.welcomeMessage()//Welcome Vikram
 // user.userName ="Sam"
 // user.welcomeMessage()//Welcome Sam
-console.log(this);
+//console.log(this);
+
+//user.welcomeMessage()
+
+//+++++++++Arrow++++++++++++++
+
+// function one() {
+//     let username = "VikramSingh"
+//     console.log(this.userName);//undefined
+// }
+// one()
+
+// const one = function() {
+//     let username = "VikramSingh"
+//     console.log(this.userName);//undefined
+// }
+// one()
+
+// const one = () => {
+//     let username = "VikramSingh"
+//     console.log(this.userName);//
+// }
+// one()
+
+// const addTwo =(num1, num2) =>{
+//     return num1 + num2
+// }
+// console.log(addTwo(5,6));
+
+
+//Implicit return
+// const addTwo1 =(num1, num2) => num1 + num2
+// console.log(addTwo1(5,6));//11
+
+const addTwo2 =(num1, num2) => (num1 + num2)
+console.log(addTwo2(5,6));//11
+
